e9d35f2b37e0ccb7377e8862a9cef6329e04eac9
2pokek <alkuzmich6479@gmail.com>
2022-10-27 00:13:36 +0300

    3

--- weather/forecast/views.py
+++ weather/forecast/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    appid = '9bf0385f33a4eeb5ba2bf7dcd7896c69'
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+    city= 'Minsk'
+    res = requests.get(url.format(city))
+    print(res.text)
+
     return render(request,'weather/index.html')
 
 def asd(request):
